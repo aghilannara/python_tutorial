11026ea9a52981c690a1ca83fcd01e559a3d99d0
Aghilan Narayanan <aghilannara@gmail.com>
2016-08-29 11:31:41 +0800

    adding calculator simple example

---
+++ calculator.py
@@ -0,0 +1,39 @@
+import pdb
+
+'''this is a calculator python program'''
+# this program is a simple calculator
+
+def addition(x,y):
+	return x+y
+
+def substraction (x,y):
+	return x-y
+
+def division(x,y):
+	return x/y
+
+def multiplication(x,y):
+	return x*y
+
+print "Welcome to calculators.py"
+print "Choose a program"
+print "1. Addition"
+print "2. Substraction"
+print "3. Multiplication"
+print "4, Division"
+print "5. Exit"
+
+client_input = int(input('Enter choice: '))
+num1 = int(input('Value of x: '))
+num2 = int(input('Value of y: '))
+
+if client_input == 1:
+	print addition(num1,num2)
+elif client_input == 2:
+	print substraction(num1,num2)
+elif client_input == 3:
+	print multiplication(num1,num2)
+elif client_input == 4:
+	print division(num1,num2)
+else:
+	exit()
